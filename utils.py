9c3d831a9661209ba03de5d7824467efce06cf39
unknown <dyatun@gmail.com>
2015-11-15 20:58:12 -0500

    Fixed thing

--- utils.py
+++ utils.py
@@ -38,30 +38,53 @@ def get_salary(first_name, last_name):
     If the teacher cannot be found, full_name is set to the given
     first_name and last_name, and salary is set to None.
     '''
-    uri = 'https://data.cityofnewyork.us/resource/k397-673e.json?first_name=%s&last_name=%s'
-    url = uri % (first_name, last_name)
+    uri = 'https://data.cityofnewyork.us/resource/k397-673e.json?first_name=%s&last_name=%s&agency_name=%s'
+    agency_name = 'DEPT OF ED PEDAGOGICAL        '
+    url = uri % (first_name, last_name, agency_name)
     request = Request(url)
     request.add_header('X-App-Token', 'wLV7RwuxVb6qKDLYtE00B2eLV')
     page = urlopen(request).read()
-    try:
-        teacher_dict = loads(page)[0]
-        salary = float(teacher_dict['base_salary'])
-        # New York has, on average, 182 school days per year and 6.59 hours per school day.
-        # Source: https://nces.ed.gov/surveys/sass/tables/sass0708_035_s1s.asp
-        if teacher_dict['pay_basis'] == ' per Day':
-            salary *= 182
-        elif teacher_dict['pay_basis'] == ' per Hour':
-            salary *= 182 * 6.59
-        first = teacher_dict['first_name'][0] + teacher_dict['first_name'][1:].lower() + ' '
-        if 'mid_init' in teacher_dict:
-            middle = teacher_dict['mid_init'] + '. '
-        else:
-            middle = ''
-        last = teacher_dict['last_name'][0] + teacher_dict['last_name'][1:].lower()
-        full_name = first + middle + last
-    except:
-        full_name = first_name + ' ' + last_name
-        salary = None
+    result = loads(page)
+    if not result:
+        agency_name = 'DEPT OF ED PEDAGOGICAL        '
+        url = uri % (first_name, last_name, agency_name)
+        request = Request(url)
+        request.add_header('X-App-Token', 'wLV7RwuxVb6qKDLYtE00B2eLV')
+        page = urlopen(request).read()
+        result = loads(page)
+        if not result:
+            agency_name = 'DEPT OF ED PER SESSION TEACHER'
+            url = uri % (first_name, last_name, agency_name)
+            request = Request(url)
+            request.add_header('X-App-Token', 'wLV7RwuxVb6qKDLYtE00B2eLV')
+            page = urlopen(request).read()
+            result = loads(page)
+            salary = float(teacher_dict['base_salary'])
+            if not result:
+                agency_name = ''
+                url = uri % (first_name, last_name, agency_name)
+                request = Request(url)
+                request.add_header('X-App-Token', 'wLV7RwuxVb6qKDLYtE00B2eLV')
+                page = urlopen(request).read()
+                result = loads(page)
+                salary = float(teacher_dict['base_salary'])
+                if not result:
+                    full_name = first_name + ' ' + last_name
+                    salary = None
+                    return {'full_name':full_name, 'salary':salary}
+    # New York has, on average, 182 school days per year and 6.59 hours per school day.
+    # Source: https://nces.ed.gov/surveys/sass/tables/sass0708_035_s1s.asp
+    if teacher_dict['pay_basis'] == ' per Day':
+        salary *= 182
+    elif teacher_dict['pay_basis'] == ' per Hour':
+        salary *= 182 * 6.59
+    first = teacher_dict['first_name'][0] + teacher_dict['first_name'][1:].lower() + ' '
+    if 'mid_init' in teacher_dict:
+        middle = teacher_dict['mid_init'] + '. '
+    else:
+        middle = ''
+    last = teacher_dict['last_name'][0] + teacher_dict['last_name'][1:].lower()
+    full_name = first + middle + last
     return {'full_name':full_name, 'salary':salary}
 
 def get_secret_key():
